d8f3cf73ed65abf7afa89fce3ae9cff06f67bb99
Darag1996 <moha.darag@yahoo.com>
2018-03-18 02:41:41 +0200

    messing around

--- database/seeds/ItemTableSeeder.php
+++ database/seeds/ItemTableSeeder.php
@@ -16,10 +16,10 @@ class ItemTableSeeder extends Seeder
     	$faker = \Faker\Factory::create();
 
                 // And now, let's create a few articles in our database:
-    	for ($i = 0; $i < 20; $i++) {
+    	for ($i = 0; $i < 10; $i++) {
     		Item::create([
     			'text' => $faker->sentence,
-    			'body' => $faker->paragraph,
+    			'body' => $faker->email,
     		]);
 
     	}
